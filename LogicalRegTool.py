25e5221828ee27615eb53558e83374f7c18a4061
matthew seyer <matthew.seyer@gmail.com>
2018-04-03 23:33:13 -0600

    Added additional interface params for debug verbosity and plugin selection

--- LogicalRegTool.py
+++ LogicalRegTool.py
@@ -3,11 +3,9 @@ import logging
 import argparse
 from lib import RegistryManager as Rm
 from lib import EnumHandlers as Eh
-from lib.plugins.PluginManager import PluginManager
+from lib.plugins import PluginManager as Pm
 
-logging.basicConfig(
-    level=logging.DEBUG
-)
+VALID_DEBUG_LEVELS = ["ERROR", "WARN", "INFO", "DEBUG"]
 
 
 def get_arguments():
@@ -22,28 +20,60 @@ def get_arguments():
         dest="source",
         action="store",
         required=True,
-        help=u"Logical source [Example: \\\\.\\C:]"
+        help="Logical source [Example: \\\\.\\C:]"
     )
     arguments.add_argument(
         "-t", "--temp_dir",
         dest="temp_dir",
         action="store",
         required=True,
-        help=u"TEMP_DIR (Make sure this is on a volume that can handle large files.)"
+        help="TEMP_DIR (Make sure this is on a volume that can handle large files.)"
+    )
+    arguments.add_argument(
+        "-p", "--plugins",
+        dest="plugins",
+        action="store",
+        default=None,
+        help="Plugins to use (comma separated list) [{}]".format(",".join(Pm.ALL_PLUGINS))
+    )
+    arguments.add_argument(
+        "--debug",
+        dest="debug",
+        action="store",
+        default="ERROR",
+        choices=VALID_DEBUG_LEVELS,
+        help="Debug level [default=ERROR]"
     )
 
     return arguments
+
+
+def set_debug_level(debug_level):
+    if debug_level in VALID_DEBUG_LEVELS:
+        logging.basicConfig(
+            level=getattr(logging, debug_level)
+        )
+    else:
+        raise(Exception("{} is not a valid debug level.".format(debug_level)))
 
 
 def main():
     arguments = get_arguments()
     options = arguments.parse_args()
 
+    set_debug_level(options.debug)
+
     tsk_img = pytsk3.Img_Info(
         options.source
     )
 
-    pm = PluginManager()
+    plist = None
+    if options.plugins:
+        plist = Pm.plugin_list_from_string(options.plugins)
+
+    pm = Pm.PluginManager(
+        plugin_list=plist
+    )
 
     reg_manager = Rm.RegistryManager()
 
--- lib/plugins/PluginManager.py
+++ lib/plugins/PluginManager.py
@@ -4,16 +4,43 @@ from lib.plugins.UserAssist import UserAssist
 from lib.plugins.AppCompat import AppCompact
 from lib.plugins.Bam import Bam
 
+PLUGIN_MAPPING = {
+    'UserAssist': UserAssist(),
+    'AppCompact': AppCompact(),
+    'Bam': Bam(),
+    'UsbEnumerator': UsbEnumerator(),
+    # 'RegistryIterator': RegistryIterator()
+}
+
+ALL_PLUGINS = [
+    'UserAssist',
+    'AppCompact',
+    'Bam',
+    'UsbEnumerator'
+]
+
+
+def plugin_list_from_string(plugin_str):
+    plugin_list = plugin_str.split(',')
+
+    for plugin in plugin_list:
+        if plugin not in PLUGIN_MAPPING:
+            raise(Exception("{} is not a valid plugin.".format(plugin)))
+
+    return plugin_list
+
 
 class PluginManager(object):
-    def __init__(self):
-        self.plugins = [
-            # RegistryIterator(),
-            UserAssist(),
-            AppCompact(),
-            Bam(),
-            UsbEnumerator()
-        ]
+    def __init__(self, plugin_list=None):
+        self.plugins = []
+
+        if not plugin_list:
+            plugin_list = ALL_PLUGINS
+
+        for plugin in plugin_list:
+            self.plugins.append(
+                PLUGIN_MAPPING[plugin]
+            )
 
     def run_plugins(self, registry_manager):
         for plugin in self.plugins:
